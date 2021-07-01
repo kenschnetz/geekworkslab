04ef908c930c6fb790a1e277f0c94a8247fad118
kenschnetz <ken@syntaxflow.com>
2021-06-30 22:41:35 -0700

    Fixed formatting issue

--- resources/views/livewire/post-list.blade.php
+++ resources/views/livewire/post-list.blade.php
@@ -5,28 +5,30 @@
     <div class="flex flex-col pt-8">
         @if(count($posts) > 0)
             @foreach($posts as $post)
-                <hr/>
-                <div class="flex items-center p-4 cursor-pointer hover:bg-gray-100" wire:key="row-{{ $post->id }}" wire:click="View('{{$post->Category->slug}}', '{{$post->slug}}')">
-                    <img class="hidden md:block object-cover float-left mr-4 mb-1 post-img" src="{{ optional($post->Images->first())->path ?? $default_image_url }}"  alt="Post image" />
-                    <div class="w-full">
-                        <div class="text-center md:text-left text-lg font-bold underline">
-                            {{ $post->title }}
-                        </div>
-                        <div class="text-center md:text-left text-lg italic">
-                            {{$post->description}}
-                        </div>
-                        <div class="md:hidden">
-                            <img class="sm-post-img border mt-3 mx-auto" src="{{ optional($post->Images->first())->path ?? $default_image_url }}"  alt="Post image"/>
-                        </div>
-                        <hr class="my-1 border-gray-100 hidden md:block" />
-                        <div class="py-0 md:py-1 mt-3 md:mt-0 text-center md:text-left text-sm">
-                            By {{ $post->User->name }}<span class="mx-3">|</span>
-                            {{$post->all_comments_count . ($post->all_comments_count === 0 || $post->all_comments_count > 1 ? ' Comments' : ' Comment')}}<span class="mx-3">|</span>
-                            {{$post->upvotes_count . ($post->upvotes_count === 0 || $post->upvotes_count > 1 ? ' Upvotes' : ' Upvote')}}
-                        </div>
-                        <hr class="my-3 md:my-1 border-gray-100" />
-                        <div class="py-1">
-                            <span>{!! Str::limit($post->content, 100) !!}</span>
+                <div wire:key="row-{{ $post->id }}">
+                    <hr/>
+                    <div class="flex items-center p-4 cursor-pointer hover:bg-gray-100" wire:click="View('{{$post->Category->slug}}', '{{$post->slug}}')">
+                        <img class="hidden md:block object-cover float-left mr-4 mb-1 post-img" src="{{ optional($post->Images->first())->path ?? $default_image_url }}"  alt="Post image" />
+                        <div class="w-full">
+                            <div class="text-center md:text-left text-lg font-bold underline">
+                                {{ $post->title }}
+                            </div>
+                            <div class="text-center md:text-left text-lg italic">
+                                {{$post->description}}
+                            </div>
+                            <div class="md:hidden">
+                                <img class="sm-post-img border mt-3 mx-auto" src="{{ optional($post->Images->first())->path ?? $default_image_url }}"  alt="Post image"/>
+                            </div>
+                            <hr class="my-1 border-gray-100 hidden md:block" />
+                            <div class="py-0 md:py-1 mt-3 md:mt-0 text-center md:text-left text-sm">
+                                By {{ $post->User->name }}<span class="mx-3">|</span>
+                                {{$post->all_comments_count . ($post->all_comments_count === 0 || $post->all_comments_count > 1 ? ' Comments' : ' Comment')}}<span class="mx-3">|</span>
+                                {{$post->upvotes_count . ($post->upvotes_count === 0 || $post->upvotes_count > 1 ? ' Upvotes' : ' Upvote')}}
+                            </div>
+                            <hr class="my-3 md:my-1 border-gray-100" />
+                            <div class="py-1">
+                                <span>{{Str::limit($post->content, 100)}}</span>
+                            </div>
                         </div>
                     </div>
                 </div>
