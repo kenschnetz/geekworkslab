98686185cc4fdca8867765e61689a6814364609f
kenschnetz <ken@syntaxflow.com>
2021-10-22 16:58:29 -0700

    Removed profile sidebar

--- resources/views/livewire/profile.blade.php
+++ resources/views/livewire/profile.blade.php
@@ -75,10 +75,10 @@
                 @endif
             </div>
         </div>
-        <div class="p-3 md:px-6 md:py-4 bg-white shadow rounded flex flex-col">
-            <p class="text-xl font-bold">Activity Feed</p>
-            <hr class="my-3" />
-            <p class="italic font-md">Feature coming soon!</p>
-        </div>
+{{--        <div class="p-3 md:px-6 md:py-4 bg-white shadow rounded flex flex-col">--}}
+{{--            <p class="text-xl font-bold">Activity Feed</p>--}}
+{{--            <hr class="my-3" />--}}
+{{--            <p class="italic font-md">Feature coming soon!</p>--}}
+{{--        </div>--}}
     </div>
 </div>
