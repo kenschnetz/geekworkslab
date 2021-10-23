10e3d39c593861ad7bc45cc58c0dafa482954fbc
kenschnetz <ken@syntaxflow.com>
2021-10-22 18:34:06 -0700

    Fixing issue where profile page shows broken post list

--- resources/views/livewire/profile.blade.php
+++ resources/views/livewire/profile.blade.php
@@ -77,11 +77,11 @@
 {{--                    <p class="italic font-md">No posts yet!</p>--}}
 {{--                @endif--}}
 {{--            </div>--}}
-{{--        </div>--}}
-        {{--        <div class="p-3 md:px-6 md:py-4 bg-white shadow rounded flex flex-col">--}}
-        {{--            <p class="text-xl font-bold">Activity Feed</p>--}}
-        {{--            <hr class="my-3" />--}}
-        {{--            <p class="italic font-md">Feature coming soon!</p>--}}
-        {{--        </div>--}}
+        </div>
+        <div class="p-3 md:px-6 md:py-4 bg-white shadow rounded flex flex-col">
+            <p class="text-xl font-bold">Activity Feed</p>
+            <hr class="my-3" />
+            <p class="italic font-md">Feature coming soon!</p>
+        </div>
     </div>
 </div>
