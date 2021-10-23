44f69d3169649f7d05700c7b414744e202e9a010
kenschnetz <ken@syntaxflow.com>
2021-10-22 18:34:34 -0700

    Fixing issue where profile page shows broken post list

--- resources/views/livewire/profile.blade.php
+++ resources/views/livewire/profile.blade.php
@@ -38,45 +38,45 @@
                 <hr class="my-3"/>
                 <p class="italic font-md">Feature coming soon!</p>
             </div>
-{{--            <div class="mt-6 p-3 bg-white shadow rounded flex flex-col md:py-4 md:px-6">--}}
-{{--                <p class="mb-3 text-xl font-bold">Posts</p>--}}
-{{--                @if(count($posts) > 0)--}}
-{{--                    @foreach($posts as $post)--}}
-{{--                        <hr/>--}}
-{{--                        <div class="flex items-center p-4 cursor-pointer hover:bg-gray-100" wire:key="row-{{ $post->id }}" wire:click="View('{{$post->Category->slug}}', '{{$post->slug}}')">--}}
-{{--                            <img class="hidden md:block object-cover float-left mr-4 mb-1 post-img" src="{{ optional($post->Images->first())->path ?? $default_image_url }}" alt="Post image"/>--}}
-{{--                            <div class="w-full">--}}
-{{--                                <p class="text-center md:text-left text-lg font-bold underline">--}}
-{{--                                    {{ $post->title }}--}}
-{{--                                </p>--}}
-{{--                                <p class="text-center md:text-left text-lg italic">--}}
-{{--                                    {{$post->description}}--}}
-{{--                                </p>--}}
-{{--                                <p class="md:hidden">--}}
-{{--                                    <img class="sm-post-img border mt-3 mx-auto" src="{{ optional($post->Images->first())->path ?? $default_image_url }}" alt="Post image"/>--}}
-{{--                                </p>--}}
-{{--                                <hr class="my-1 border-gray-100 hidden md:block"/>--}}
-{{--                                <p class="py-0 md:py-1 mt-3 md:mt-0 text-center md:text-left text-sm">--}}
-{{--                                    By--}}
-{{--                                    <a class="underline hover:no-underline" href="{{ route('profile', ['user_id' => $post->User->id]) }}">{{ $post->User->name }}</a><span class="mx-3">|</span>--}}
-{{--                                    {{$post->all_comments_count . ($post->all_comments_count === 0 || $post->all_comments_count > 1 ? ' Comments' : ' Comment')}}--}}
-{{--                                    <span class="mx-3">|</span>--}}
-{{--                                    {{$post->upvotes_count . ($post->upvotes_count === 0 || $post->upvotes_count > 1 ? ' Upvotes' : ' Upvote')}}--}}
-{{--                                </p>--}}
-{{--                                <hr class="my-3 md:my-1 border-gray-100"/>--}}
-{{--                                <p class="py-1">--}}
-{{--                                    <span>{!! Str::limit($post->content, 100) !!}</span>--}}
-{{--                                </p>--}}
-{{--                            </div>--}}
-{{--                        </div>--}}
-{{--                    @endforeach--}}
-{{--                    <hr class="my-3"/>--}}
-{{--                    {{ $posts->links() }}--}}
-{{--                @else--}}
-{{--                    <hr class="my-3"/>--}}
-{{--                    <p class="italic font-md">No posts yet!</p>--}}
-{{--                @endif--}}
-{{--            </div>--}}
+            <div class="mt-6 p-3 bg-white shadow rounded flex flex-col md:py-4 md:px-6">
+                <p class="mb-3 text-xl font-bold">Posts</p>
+                @if(count($posts) > 0)
+                    @foreach($posts as $post)
+                        <hr/>
+                        <div class="flex items-center p-4 cursor-pointer hover:bg-gray-100" wire:key="row-{{ $post->id }}" wire:click="View('{{$post->Category->slug}}', '{{$post->slug}}')">
+                            <img class="hidden md:block object-cover float-left mr-4 mb-1 post-img" src="{{ optional($post->Images->first())->path ?? $default_image_url }}" alt="Post image"/>
+                            <div class="w-full">
+                                <p class="text-center md:text-left text-lg font-bold underline">
+                                    {{ $post->title }}
+                                </p>
+                                <p class="text-center md:text-left text-lg italic">
+                                    {{$post->description}}
+                                </p>
+                                <p class="md:hidden">
+                                    <img class="sm-post-img border mt-3 mx-auto" src="{{ optional($post->Images->first())->path ?? $default_image_url }}" alt="Post image"/>
+                                </p>
+                                <hr class="my-1 border-gray-100 hidden md:block"/>
+                                <p class="py-0 md:py-1 mt-3 md:mt-0 text-center md:text-left text-sm">
+                                    By
+                                    <a class="underline hover:no-underline" href="{{ route('profile', ['user_id' => $post->User->id]) }}">{{ $post->User->name }}</a><span class="mx-3">|</span>
+                                    {{$post->all_comments_count . ($post->all_comments_count === 0 || $post->all_comments_count > 1 ? ' Comments' : ' Comment')}}
+                                    <span class="mx-3">|</span>
+                                    {{$post->upvotes_count . ($post->upvotes_count === 0 || $post->upvotes_count > 1 ? ' Upvotes' : ' Upvote')}}
+                                </p>
+                                <hr class="my-3 md:my-1 border-gray-100"/>
+                                <p class="py-1">
+                                    <span>{!! Str::limit($post->content, 100) !!}</span>
+                                </p>
+                            </div>
+                        </div>
+                    @endforeach
+                    <hr class="my-3"/>
+                    {{ $posts->links() }}
+                @else
+                    <hr class="my-3"/>
+                    <p class="italic font-md">No posts yet!</p>
+                @endif
+            </div>
         </div>
         <div class="p-3 md:px-6 md:py-4 bg-white shadow rounded flex flex-col">
             <p class="text-xl font-bold">Activity Feed</p>
