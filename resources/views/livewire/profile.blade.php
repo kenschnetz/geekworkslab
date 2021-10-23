ae909ee5f3b65656fb6e5824a5948b18aca5cda1
kenschnetz <ken@syntaxflow.com>
2021-10-22 19:08:30 -0700

    Fixing issue where profile page shows broken post list

--- resources/views/livewire/profile.blade.php
+++ resources/views/livewire/profile.blade.php
@@ -42,31 +42,31 @@
                 <p class="mb-3 text-xl font-bold">Posts</p>
                 @if(count($posts) > 0)
                     @foreach($posts as $post)
-                        <hr/>
-                        <div class="flex items-center p-4 cursor-pointer hover:bg-gray-100" wire:key="row-{{ $post->id }}" wire:click="View('{{$post->Category->slug}}', '{{$post->slug}}')">
-                            <img class="hidden md:block object-cover float-left mr-4 mb-1 post-img" src="{{ optional($post->Images->first())->path ?? $default_image_url }}" alt="Post image"/>
-                            <div class="w-full">
-                                <p class="text-center md:text-left text-lg font-bold underline">
-                                    {{ $post->title }}
-                                </p>
-                                <p class="text-center md:text-left text-lg italic">
-                                    {{$post->description}}
-                                </p>
-                                <p class="md:hidden">
-                                    <img class="sm-post-img border mt-3 mx-auto" src="{{ optional($post->Images->first())->path ?? $default_image_url }}" alt="Post image"/>
-                                </p>
-                                <hr class="my-1 border-gray-100 hidden md:block"/>
-                                <p class="py-0 md:py-1 mt-3 md:mt-0 text-center md:text-left text-sm">
-                                    By
-                                    <a class="underline hover:no-underline" href="{{ route('profile', ['user_id' => $post->User->id]) }}">{{ $post->User->name }}</a><span class="mx-3">|</span>
-                                    {{$post->all_comments_count . ($post->all_comments_count === 0 || $post->all_comments_count > 1 ? ' Comments' : ' Comment')}}
-                                    <span class="mx-3">|</span>
-                                    {{$post->upvotes_count . ($post->upvotes_count === 0 || $post->upvotes_count > 1 ? ' Upvotes' : ' Upvote')}}
-                                </p>
-                                <hr class="my-3 md:my-1 border-gray-100"/>
-                                <p class="py-1">
-                                    <div>{{ Str::limit($post->content, 100) }}</div>
-                                </p>
+                        <div wire:key="row-{{ $post->id }}">
+                            <hr/>
+                            <div class="flex items-center p-4 cursor-pointer hover:bg-gray-100" wire:click="View('{{$post->Category->slug}}', '{{$post->slug}}')">
+                                <img class="hidden md:block object-cover float-left mr-4 mb-1 post-img" src="{{ optional($post->Images->first())->path ?? $default_image_url }}"  alt="Post image" />
+                                <div class="w-full">
+                                    <div class="text-center md:text-left text-lg font-bold underline">
+                                        {{ $post->title }}
+                                    </div>
+                                    <div class="text-center md:text-left text-lg italic">
+                                        {{$post->description}}
+                                    </div>
+                                    <div class="md:hidden">
+                                        <img class="sm-post-img border mt-3 mx-auto" src="{{ optional($post->Images->first())->path ?? $default_image_url }}"  alt="Post image"/>
+                                    </div>
+                                    <hr class="my-1 border-gray-100 hidden md:block" />
+                                    <div class="py-0 md:py-1 mt-3 md:mt-0 text-center md:text-left text-sm">
+                                        By {{ $post->User->name }}<span class="mx-3">|</span>
+                                        {{$post->all_comments_count . ($post->all_comments_count === 0 || $post->all_comments_count > 1 ? ' Comments' : ' Comment')}}<span class="mx-3">|</span>
+                                        {{$post->upvotes_count . ($post->upvotes_count === 0 || $post->upvotes_count > 1 ? ' Upvotes' : ' Upvote')}}
+                                    </div>
+{{--                                    <hr class="my-3 md:my-1 border-gray-100" />--}}
+{{--                                    <div class="py-1">--}}
+{{--                                        {!! Str::limit($post->content, 120) !!}--}}
+{{--                                    </div>--}}
+                                </div>
                             </div>
                         </div>
                     @endforeach
