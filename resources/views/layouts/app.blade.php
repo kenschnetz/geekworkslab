5b3abd8dbcae9036d8adc55eed3eabca965434b8
Ken <ken@syntaxflow.com>
2021-11-29 18:59:19 -0800

    Testing iOS issue bugfix

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -14,7 +14,7 @@
         <!-- Scripts -->
         <script>
             (function() {
-                document.addEventListener("DOMContentLoaded", function(event) {
+                window.addEventListener("DOMContentLoaded", function(event) {
                     var scrollpos = localStorage.getItem('scrollpos');
                     if (scrollpos) window.scrollTo(0, scrollpos);
                 });
